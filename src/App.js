694655e12106c6ea96ecdba4398a1d0672278d16
vladsemenets <vladmillionaire@gmail.com>
2018-01-08 12:28:42 +0200

    Added the ability to Log in and create users with showing an error if something goes wrong

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import firebase from 'firebase';
 import reducers from './reducers';
 import LoginForm from './components/LoginForm';
+import ReduxThunk from 'redux-thunk';
 
 
 
@@ -18,11 +18,13 @@ class App extends Component {
             storageBucket: "manager-a8fa4.appspot.com",
             messagingSenderId: "650117483282"
           };
-          firebase.initializeApp(config);
+        firebase.initializeApp(config);
     }
     render() {
+        const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
         return (
-            <Provider store={createStore(reducers)}>
+            <Provider store={store}>
                 <LoginForm />
             </Provider>
         );
